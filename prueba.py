2ab05474d661a884159b64965e30a93632079c19
sgomezleon <sergom19@hotmail.com>
2017-01-25 20:21:45 +0100

    Probando probando

---
+++ prueba.py
@@ -0,0 +1,13 @@
+#Fichero de prueba con GitHub
+
+#fork (via web) 		//Se copia el repositorio de la web
+
+#git clone [URL]
+#git pull	//Si ya tengo una copia del trabajo
+
+#git add holamundo.py			// Opcion A
+#git commit -m "mensaje de cambio"  	// Opcion A
+
+#git commit -m "mensaje de cambio" holamundo.py   // Opcion B
+
+#git push 	//GitHub pedirá usuario y contaseña. Sube el ejercicio a la web
